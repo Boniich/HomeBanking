5b3ee52181cf060b77bb2cb3f53f4f9ed0c0adb0
luiggylml <luiggylml@gmail.com>
2022-02-19 16:27:42 -0500

    create components and views(my-homebanking/src/components/)

--- my-homebanking/src/App.jsx
+++ my-homebanking/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
-
   return (
     <div className="App">
+      <h1>hola</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
