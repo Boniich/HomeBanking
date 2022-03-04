09dc616a346cd9d138a5b4a139a145dc1670ff1f
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-03-04 18:49:11 -0300

    feat(route): agrega todo lo referente al enrutado de paginas basico

--- my-homebanking/src/App.jsx
+++ my-homebanking/src/App.jsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/views/layout/Layout";
 import LoginView from "./components/views/login/LoginView";
 
 function App() {
   return (
-    <div className="App">
-      <LoginView />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        {/* Ruta publica */}
+        <Route path="login" element={<LoginView />}/>
+
+        {/* Se deben proteger mas adelante */}
+        <Route path="dashboard" element={<h1>DashBoard</h1>}/>
+        {/* Este ruta es provisoria, mas adelante vere si realmente necesito que este aca */}
+        <Route path="transferencias" element={<h1>transferencias</h1>}/>
+        {/* error 404 (si no encuentra la pagina) */}
+        <Route path="*" element={<h1>Pagina no encontrada</h1>}/>
+
+      </Route>
+    </Routes>
+
   );
 }
 
