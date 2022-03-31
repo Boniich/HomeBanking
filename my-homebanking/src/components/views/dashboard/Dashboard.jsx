1792bc6482fa7d783c5cd63ea5496f91dcd4290b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-03-31 19:52:33 -0300

    add: add nav bar, header and transference activity

---
+++ my-homebanking/src/components/views/dashboard/Dashboard.jsx
@@ -0,0 +1,104 @@
+import {
+  Li,
+  ListMenu,
+  MenuIcons,
+  Nav,
+  Section,
+  Ul,
+  Link,
+  BoxUser,
+  UlMobile,
+  Span,
+  ArrowIcon,
+  CloseIcon,
+  ActivityContainer,
+  ActivityContainer2,
+} from "./styleDashBoard";
+import {
+  faHouseChimney,
+  faRightLeft,
+  faAngleRight,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
+import { FooterText } from "../../../theme/footer/footer";
+import { HeadingSemiBold5 } from "../../../theme/heading/heading";
+import { ParagraphMedium2, ParagraphMedium3, ParagraphSemibold2 } from "../../../theme/paragraph/paragraph";
+const Dashboard = () => {
+  return (
+    <Section>
+      <BoxUser>
+        <div className="box1">
+          <HeadingSemiBold5>Hola, Darlene</HeadingSemiBold5>
+          <ParagraphMedium2>Bienvenida a tu banca movil</ParagraphMedium2>
+        </div>
+      </BoxUser>
+
+      <ActivityContainer>
+        {/* cambiar nombre del div mas adelante */}
+        <ActivityContainer2>
+          <ParagraphMedium2>Actividades Recientes</ParagraphMedium2>
+          <div>
+            <figure>
+              <ParagraphMedium2>Transf Web</ParagraphMedium2>
+              <ParagraphSemibold2>-50</ParagraphSemibold2>
+              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
+            </figure>
+          </div>
+
+          <div>
+            <ParagraphMedium2>Ver toda la actividad</ParagraphMedium2>
+          </div>
+        </ActivityContainer2>
+      </ActivityContainer>
+
+      <Nav>
+        <Ul>
+          <Li>
+            <Link>
+              <span>
+                <MenuIcons icon={faHouseChimney} />
+              </span>
+              <span>
+                <FooterText>Inicio</FooterText>
+              </span>
+            </Link>
+          </Li>
+          <Li>
+            <Link>
+              <MenuIcons icon={faRightLeft} />
+              <span>
+                <FooterText>Transacciones</FooterText>
+              </span>
+            </Link>
+          </Li>
+          <Li>
+            <Link>
+              Iconito
+              <span>
+                <FooterText>User</FooterText>
+              </span>
+            </Link>
+          </Li>
+        </Ul>
+      </Nav>
+      {/* <ListMenu>
+                    <div className="box-close-btn">
+                        <span className="span-close-btn">
+                        <CloseIcon icon={faX}></CloseIcon>
+                        </span>
+                    </div>
+                    <UlMobile>
+    
+                        <Li><Span>Tarjetas <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
+                        <Li><Span>Transferencias <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
+                        <Li><Span>Configuración <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
+                        <Li><Span>Ayuda <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
+                        <Li><Span>Cerrar sesión <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
+                    </UlMobile>
+                    
+                </ListMenu> */}
+    </Section>
+  );
+};
+
+export default Dashboard;
---
+++ my-homebanking/src/components/views/dashboard/styleDashBoard.js
@@ -0,0 +1,159 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import { neutralColor } from "../../../theme/colors/colors";
+import { HeadingSemiBold5 } from "../../../theme/heading/heading";
+import { ParagraphMedium2 } from "../../../theme/paragraph/paragraph";
+import { shadownMD } from "../../../theme/shadown/shadown";
+
+const Nav = styled.nav`
+  position: fixed;
+  bottom: 0;
+  /* top: 0; */
+  /* desktop */
+  /* position: fixed; */
+  /* bottom: none; */
+  /* top: 0; */
+  background-color: #ffffff;
+  box-shadow: 0px -1px 16px rgba(17, 24, 39, 0.06);
+  height: 64px;
+  width: 100%;
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Li = styled.li``;
+
+const UlMobile = styled(Ul)`
+  flex-direction: column;
+  display: flex;
+  justify-content: space-between;
+  ${Li} {
+    padding: 10px 24px;
+  }
+`;
+
+const Link = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 64px;
+  text-align: center;
+`;
+
+const MenuIcons = styled(FontAwesomeIcon)`
+  width: 24px;
+  height: 24px;
+`;
+
+const ArrowIcon = styled(FontAwesomeIcon)`
+  width: 16px;
+  height: 16px;
+  color: ${neutralColor.neutral300};
+`;
+
+const Section = styled.section`
+  background-color: ${neutralColor.neutral50};
+`;
+
+const ListMenu = styled.div`
+  position: absolute;
+  top: 0;
+  background-color: #fff;
+  height: 100%;
+  width: 100%;
+
+  .box-close-btn {
+    margin-bottom: 44px;
+    margin-top: 32px;
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 36px;
+  }
+
+  .span-close-btn {
+    width: 28px;
+    height: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${neutralColor.neutral100};
+    border-radius: 100%;
+  }
+`;
+
+const CloseIcon = styled(FontAwesomeIcon)`
+  width: 9.33px;
+  height: 9.33px;
+  color: ${neutralColor.neutral700};
+`;
+
+const BoxUser = styled.div`
+  background-color: ${neutralColor.neutral700};
+  width: 100%;
+  height: 216px;
+  ${HeadingSemiBold5} {
+    margin: 0;
+    color: #ffffff;
+    font-weight: 600;
+  }
+
+  ${ParagraphMedium2} {
+    color: #ffffff;
+    margin: 0;
+  }
+  .box1 {
+    padding: 48px 0 0 24px;
+  }
+`;
+
+const Span = styled.span`
+  display: flex;
+  justify-content: space-between;
+`;
+
+// actividad
+
+//contendor actividad
+
+const ActivityContainer = styled.div`
+  width: 312px;
+  margin: 0 auto;
+  border-radius: 20px;
+  box-shadow: ${shadownMD};
+`;
+
+const ActivityContainer2 = styled.div`
+  padding: 24px 24px 40px;
+  ${ParagraphMedium2} {
+    margin: 0;
+  }
+`;
+
+// Cards
+
+// contenedor Cards
+
+export {
+  Section,
+  Nav,
+  Ul,
+  Li,
+  Link,
+  MenuIcons,
+  ListMenu,
+  BoxUser,
+  UlMobile,
+  Span,
+  ArrowIcon,
+  CloseIcon,
+  ActivityContainer,
+  ActivityContainer2,
+};
