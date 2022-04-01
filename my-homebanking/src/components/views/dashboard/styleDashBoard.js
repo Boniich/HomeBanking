5ef27e656b6081f8f5ed9caca605c4fa0a8509cc
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-01 19:33:38 -0300

    feat: add style for all dashBoard

--- my-homebanking/src/components/views/dashboard/styleDashBoard.js
+++ my-homebanking/src/components/views/dashboard/styleDashBoard.js
@@ -2,7 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { neutralColor } from "../../../theme/colors/colors";
 import { HeadingSemiBold5 } from "../../../theme/heading/heading";
-import { ParagraphMedium2 } from "../../../theme/paragraph/paragraph";
+import {
+  ParagraphMedium2,
+  ParagraphMedium3,
+  ParagraphSemibold2,
+} from "../../../theme/paragraph/paragraph";
 import { shadownMD } from "../../../theme/shadown/shadown";
 
 const Nav = styled.nav`
@@ -126,6 +130,7 @@ const Span = styled.span`
 const ActivityContainer = styled.div`
   width: 312px;
   margin: 0 auto;
+  margin-bottom: 100px;
   border-radius: 20px;
   box-shadow: ${shadownMD};
 `;
@@ -140,6 +145,42 @@ const ActivityContainer2 = styled.div`
 // Cards
 
 // contenedor Cards
+
+const CardTransfContainer = styled.div`
+  margin: 8px 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const FigureTransf = styled.figure`
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
+const TransfInfo = styled.div`
+  ${ParagraphSemibold2} {
+    margin-bottom: 4px;
+  }
+  ${ParagraphMedium3} {
+    margin: 0;
+  }
+`;
+
+const Transf = styled.div``;
+
+const Divider = styled.hr`
+  margin: 0;
+  padding: 0;
+  background-color: ${neutralColor.neutral200};
+`;
+
+const AllActivityBox = styled.div`
+  margin-top: 32px;
+`;
 
 export {
   Section,
@@ -156,4 +197,10 @@ export {
   CloseIcon,
   ActivityContainer,
   ActivityContainer2,
+  CardTransfContainer,
+  FigureTransf,
+  TransfInfo,
+  Divider,
+  Transf,
+  AllActivityBox,
 };
