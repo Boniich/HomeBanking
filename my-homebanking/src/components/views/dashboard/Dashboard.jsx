8da67903b213f51255e32d26202737ea8d4d7bac
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-01 19:44:10 -0300

    refactor: removed all pure code about transf and add components with this code in parts

--- my-homebanking/src/components/views/dashboard/Dashboard.jsx
+++ my-homebanking/src/components/views/dashboard/Dashboard.jsx
@@ -29,6 +29,7 @@ import {
 import { FooterText } from "../../../theme/footer/footer";
 import { HeadingSemiBold5 } from "../../../theme/heading/heading";
 import { ParagraphMedium2, ParagraphMedium3, ParagraphSemibold2 } from "../../../theme/paragraph/paragraph";
+import TransferenceView from "./transferences/TransferenceView";
 const Dashboard = () => {
   return (
     <Section>
@@ -38,112 +39,7 @@ const Dashboard = () => {
           <ParagraphMedium2>Bienvenida a tu banca movil</ParagraphMedium2>
         </div>
       </BoxUser>
-      <ActivityContainer>
-        {/* cambiar nombre del div mas adelante */}
-        <ActivityContainer2>
-          <ParagraphMedium2>Actividades Recientes</ParagraphMedium2>
-          <CardTransfContainer>
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            <Transf>
-            <FigureTransf>
-              <TransfInfo>
-              <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-              <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
-              </TransfInfo>
-              <ParagraphSemibold2>-50</ParagraphSemibold2>
-            </FigureTransf>
-            <div><Divider/></div>
-            </Transf>
-
-            
-          </CardTransfContainer>
-
-          
-
-          <AllActivityBox>
-          <ParagraphMedium2>Ver toda la actividad</ParagraphMedium2>
-          </AllActivityBox>
-            
-
-        </ActivityContainer2>
-      </ActivityContainer>
-
+      <TransferenceView/>
       <Nav>
         <Ul>
           <Li>
