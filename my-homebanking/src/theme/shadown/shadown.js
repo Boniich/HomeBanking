1f91d67743282fc596b90696110ce9e081e7e3ef
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-29 11:26:43 -0300

    files deleted

--- my-homebanking/src/theme/shadown/shadown.js
+++
@@ -1,7 +0,0 @@
-const shadownXS = `0px 2px 8px rgba(16,71,62,0.11)`;
-const shadownSM = `0px 6px 16px -1px rgba(16,71,62,0.11)`;
-const shadownMD = `0px 9px 20px -2px rgba(16,71,62,0.13)`;
-const shadownLG = `0px 12px 37px -2px rgba(16,71,62,0.09)`;
-const shadownXL = `0px 18px 56px -2px rgba(16,71,62,0.13)`;
-
-export { shadownXS, shadownXL, shadownSM, shadownMD, shadownLG };
