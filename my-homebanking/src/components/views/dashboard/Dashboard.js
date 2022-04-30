5896924eea62e57eab616cb92aaab91d30e6684a
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-04-29 21:09:10 -0300

    feat: add number account, copy button and a button for change of type of saving

--- my-homebanking/src/components/views/dashboard/Dashboard.js
+++ my-homebanking/src/components/views/dashboard/Dashboard.js
@@ -15,46 +15,62 @@ import {
   UserSection,
   UserSection2,
   ListaItems,
+  SavingType,
+  NumberAccount,
+  CopyButton,
 } from "./styleDashBoard";
 import {
   faHouseChimney,
   faRightLeft,
   faAngleRight,
   faX,
+  faCopy,
 } from "@fortawesome/free-solid-svg-icons";
 import { FooterText } from "../../../theme/footer/footer";
 import TransferenceView from "./transferences/TransferenceView";
 import WelcomeHeader from "./welcomeHeader/WelcomeHeader";
-import { ParagraphMedium3 } from "../../../theme/paragraph/paragraph";
+import {
+  ParagraphMedium2,
+  ParagraphMedium3,
+  ParagraphSemibold3,
+} from "../../../theme/paragraph/paragraph";
 import { HeadingSemiBold3 } from "../../../theme/heading/heading";
 import { Button } from "../../../theme/buttons/buttons";
 import { useState } from "react";
 const Dashboard = () => {
   const [responsiveNav, setResponsiveNav] = useState(false);
 
-  const openResponsiveNav = () =>{
+  const openResponsiveNav = () => {
     setResponsiveNav(true);
-  }
+  };
 
-  const closeResponsivenav = () =>{
+  const closeResponsivenav = () => {
     setResponsiveNav(false);
-  }
+  };
 
   return (
     <Section>
       <WelcomeHeader />
-    <UserSection> 
-    <UserSection2>
-    <AccountSummary>
-        <AccountSummaryContent>
-          <ParagraphMedium3>Saldo Disponible</ParagraphMedium3>
-          <HeadingSemiBold3>$200.00</HeadingSemiBold3>
-          <Button>Enviar Dinero</Button>
-          </AccountSummaryContent>
-        </AccountSummary>
-      <TransferenceView/>
-    </UserSection2>
-    </UserSection>
+      <UserSection>
+        <UserSection2>
+          <AccountSummary>
+            <AccountSummaryContent>
+              {/* el tipo de ahorro cambia segun lo seleccionado */}
+              <SavingType>
+                <ParagraphMedium3>Ahorro en Dolares</ParagraphMedium3>
+                <ParagraphSemibold3>Cambiar</ParagraphSemibold3>
+              </SavingType>
+              <HeadingSemiBold3>$200.00</HeadingSemiBold3>
+              <NumberAccount>
+                <ParagraphMedium2>12458745893254</ParagraphMedium2>
+                <CopyButton icon={faCopy} />
+              </NumberAccount>
+              <Button>Enviar Dinero</Button>
+            </AccountSummaryContent>
+          </AccountSummary>
+          <TransferenceView />
+        </UserSection2>
+      </UserSection>
 
       <Nav>
         <Ul>
@@ -86,23 +102,43 @@ const Dashboard = () => {
           </Li>
         </Ul>
       </Nav>
-      {responsiveNav &&
-      <ListMenu>
-                    <div className="box-close-btn">
-                        <span className="span-close-btn">
-                        <CloseIcon icon={faX} onClick={closeResponsivenav}></CloseIcon>
-                        </span>
-                    </div>
-                    <UlMobile>
-    
-                        <Li><Span><ListaItems>Tarjetas</ListaItems><ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
-                        <Li><Span>Transferencias <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
-                        <Li><Span>Configuración <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
-                        <Li><Span>Ayuda <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
-                        <Li><Span>Cerrar sesión <ArrowIcon icon={faAngleRight}></ArrowIcon></Span></Li>
-                    </UlMobile>
-                    
-                </ListMenu>}
+      {responsiveNav && (
+        <ListMenu>
+          <div className="box-close-btn">
+            <span className="span-close-btn">
+              <CloseIcon icon={faX} onClick={closeResponsivenav}></CloseIcon>
+            </span>
+          </div>
+          <UlMobile>
+            <Li>
+              <Span>
+                <ListaItems>Tarjetas</ListaItems>
+                <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </Li>
+            <Li>
+              <Span>
+                Transferencias <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </Li>
+            <Li>
+              <Span>
+                Configuración <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </Li>
+            <Li>
+              <Span>
+                Ayuda <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </Li>
+            <Li>
+              <Span>
+                Cerrar sesión <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </Li>
+          </UlMobile>
+        </ListMenu>
+      )}
     </Section>
   );
 };
