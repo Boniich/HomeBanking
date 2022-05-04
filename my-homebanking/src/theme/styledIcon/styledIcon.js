88bd241b1bf6c2bfd125090973b5d1e2ee6b7f45
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-04 13:39:27 -0300

    feat: add two new icons

--- my-homebanking/src/theme/styledIcon/styledIcon.js
+++ my-homebanking/src/theme/styledIcon/styledIcon.js
@@ -1,7 +1,7 @@
 // This are components that handle icons for will use many times
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { neutralColor } from "../colors/colors";
+import { neutralColor, primaryColor } from "../colors/colors";
 
 const CloseIcon = styled(FontAwesomeIcon)`
   width: 9.33px;
@@ -13,4 +13,15 @@ const ArrowIcon = styled(FontAwesomeIcon)`
   height: 16px;
   color: ${neutralColor.neutral300};
 `;
-export { CloseIcon, ArrowIcon };
+
+const SendIcon = styled(FontAwesomeIcon)`
+  font-weight: 13.33px;
+  color: #fff;
+`;
+
+const SendArrowIcon = styled(ArrowIcon)`
+  color: ${primaryColor.primary500};
+  font-size: 14px;
+  margin-left: 64px;
+`;
+export { CloseIcon, ArrowIcon, SendIcon, SendArrowIcon };
