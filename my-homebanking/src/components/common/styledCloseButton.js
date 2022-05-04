835d07c2ef6a82992109c46a89e611b24fe1091b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-04 09:31:45 -0300

    feat: add close button as re usable component

---
+++ my-homebanking/src/components/common/styledCloseButton.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { neutralColor } from "../../theme/colors/colors";
+
+const CloseButton = styled.div`
+  margin-bottom: 44px;
+  margin-top: 32px;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 36px;
+`;
+
+const CloseButtonContent = styled.span`
+  width: 28px;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${neutralColor.neutral100};
+  border-radius: 100%;
+`;
+
+export { CloseButton, CloseButtonContent };
