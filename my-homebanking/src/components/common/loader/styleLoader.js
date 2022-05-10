6123e16e3c16ab269baa6b37459e79607bbad99a
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-10 19:23:11 -0300

    feat: add re usable loader component

---
+++ my-homebanking/src/components/common/loader/styleLoader.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Svg = styled.svg`
+  width: 100%;
+  height: 55px;
+  text-align: center;
+  margin: 0;
+  /* margin: 20px; */
+  display: inline-block;
+`;
+
+export { Svg };
