e0ad08aca2bba26c675e2652de19dfee0e1f36eb
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-11 18:36:57 -0300

    feat: add some css changes

--- my-homebanking/src/components/views/dashboard/welcomeHeader/StyleWelcomeHeader.js
+++ my-homebanking/src/components/views/dashboard/welcomeHeader/StyleWelcomeHeader.js
@@ -2,10 +2,18 @@ import styled from "styled-components";
 import { neutralColor } from "../../../../theme/colors/colors";
 import { HeadingSemiBold5 } from "../../../../theme/heading/heading";
 import { ParagraphMedium2 } from "../../../../theme/paragraph/paragraph";
-export const BoxUser = styled.div`
+
+export const WelcomeUserTexs = styled.div`
+  width: 320px;
+  margin: 0 auto;
+  padding: 48px 0 0 24px;
+`;
+
+export const WelcomeUserContainer = styled.div`
   background-color: ${neutralColor.neutral700};
   width: 100%;
   height: 216px;
+
   ${HeadingSemiBold5} {
     margin: 0;
     color: #ffffff;
@@ -16,7 +24,20 @@ export const BoxUser = styled.div`
     color: #ffffff;
     margin: 0;
   }
-  .box1 {
-    padding: 48px 0 0 24px;
+
+  @media screen and (min-width: 744px) {
+    height: 328px;
+
+    ${WelcomeUserTexs} {
+      width: 560px;
+    }
+  }
+
+  @media screen and (min-width: 1512px) {
+    height: 460px;
+
+    ${WelcomeUserTexs} {
+      width: 1060px;
+    }
   }
 `;
