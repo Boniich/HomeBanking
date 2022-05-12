7b033352b573a22a7a2c003b91ff846e33fe474b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-12 19:54:42 -0300

    refactor: add prop to component

--- my-homebanking/src/components/views/dashboard/transferences/TransferenceCardView.js
+++ my-homebanking/src/components/views/dashboard/transferences/TransferenceCardView.js
@@ -1,16 +1,18 @@
-import { ParagraphMedium3, ParagraphSemibold2 } from "../../../../theme/paragraph/paragraph";
+import {
+  ParagraphMedium3,
+  ParagraphSemibold2,
+} from "../../../../theme/paragraph/paragraph";
 import { Divider, FigureTransf, Transf, TransfInfo } from "./styleTransference";
 
-
-const TransferenceCardView = () => {
+const TransferenceCardView = ({ amount, motive, date }) => {
   return (
     <Transf>
       <FigureTransf>
         <TransfInfo>
-          <ParagraphSemibold2>Transf Web</ParagraphSemibold2>
-          <ParagraphMedium3>08 Ene.2022 - 04:21pm</ParagraphMedium3>
+          <ParagraphSemibold2>{motive}</ParagraphSemibold2>
+          <ParagraphMedium3>{date}</ParagraphMedium3>
         </TransfInfo>
-        <ParagraphSemibold2>-50</ParagraphSemibold2>
+        <ParagraphSemibold2>{amount}</ParagraphSemibold2>
       </FigureTransf>
       <div>
         <Divider />
