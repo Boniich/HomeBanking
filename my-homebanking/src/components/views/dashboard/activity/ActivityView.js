a951655b4d7fa973811ae1b5a402034b31f6bf1e
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-13 11:19:20 -0300

    refactor: refactor code in order by it be more clean

--- my-homebanking/src/components/views/dashboard/activity/ActivityView.js
+++ my-homebanking/src/components/views/dashboard/activity/ActivityView.js
@@ -1,16 +1,6 @@
-import { Link } from "react-router-dom";
-import {
-  ParagraphMedium2,
-  ParagraphUnderline2,
-} from "../../../../theme/paragraph/paragraph";
+import { ExistsActivity } from "./existsActivity/ExistsActivity";
 import { NonActivityMsg } from "./nonActivityMsg/NonActivityMsg";
-import {
-  ActivityContainer,
-  RenderActivity,
-  AllActivityBox,
-  CardTransfContainer,
-} from "./styleActivityView";
-import TransferenceCardView from "./transference/TransferenceCardView";
+import { ActivityContainer } from "./styleActivityView";
 
 const ActivityView = () => {
   const transferenceMockData = [
@@ -35,31 +25,31 @@ const ActivityView = () => {
     {
       id: 4,
       amount: 500,
-      motive: "Deuda 3",
+      motive: "Deuda 4",
       date: "2022-03-01",
     },
     {
       id: 5,
       amount: 500,
-      motive: "Deuda 3",
+      motive: "Deuda 5",
       date: "2022-03-01",
     },
     {
       id: 6,
       amount: 500,
-      motive: "Deuda 3",
+      motive: "Deuda 6",
       date: "2022-03-01",
     },
     {
       id: 7,
       amount: 500,
-      motive: "Deuda 3",
+      motive: "Deuda 7",
       date: "2022-03-01",
     },
     {
       id: 8,
       amount: 500,
-      motive: "Deuda 3",
+      motive: "Deuda 8",
       date: "2022-03-01",
     },
   ];
@@ -67,24 +57,7 @@ const ActivityView = () => {
   return (
     <ActivityContainer>
       {transferenceMockData.length > 0 ? (
-        <RenderActivity>
-          <ParagraphMedium2>Actividad Reciente</ParagraphMedium2>
-          <CardTransfContainer>
-            {transferenceMockData.map((transference) => (
-              <TransferenceCardView
-                key={transference.id}
-                amount={transference.amount}
-                motive={transference.motive}
-                date={transference.date}
-              />
-            ))}
-          </CardTransfContainer>
-          <AllActivityBox>
-            <Link to="/transacciones">
-              <ParagraphUnderline2>Ver toda la actividad</ParagraphUnderline2>
-            </Link>
-          </AllActivityBox>
-        </RenderActivity>
+        <ExistsActivity transferenceMockData={transferenceMockData} />
       ) : (
         <NonActivityMsg />
       )}
--- my-homebanking/src/components/views/dashboard/activity/styleActivityView.js
+++ my-homebanking/src/components/views/dashboard/activity/styleActivityView.js
@@ -1,9 +1,4 @@
 import styled from "styled-components";
-import { primaryColor } from "../../../../theme/colors/colors";
-import {
-  ParagraphMedium2,
-  ParagraphUnderline2,
-} from "../../../../theme/paragraph/paragraph";
 import { shadownMD, shadownXS } from "../../../../theme/shadown/shadown";
 
 const ActivityContainer = styled.div`
@@ -23,35 +18,4 @@ const ActivityContainer = styled.div`
   }
 `;
 
-const RenderActivity = styled.div`
-  padding: 24px 24px 40px;
-  ${ParagraphMedium2} {
-    margin: 0;
-  }
-
-  @media screen and (min-width: 744px) {
-    padding: 32px 32px 40px;
-  }
-`;
-
-const CardTransfContainer = styled.div`
-  margin: 8px 0;
-  display: flex;
-  flex-direction: column;
-`;
-
-const AllActivityBox = styled.div`
-  margin-top: 32px;
-
-  ${ParagraphUnderline2} {
-    color: ${primaryColor.primary600};
-    font-weight: 400;
-  }
-`;
-
-export {
-  ActivityContainer,
-  RenderActivity,
-  CardTransfContainer,
-  AllActivityBox,
-};
+export { ActivityContainer };
