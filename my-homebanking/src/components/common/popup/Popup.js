c00ef8268ccfd4d7982a050635f7e1990f478b63
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-19 15:43:21 -0300

    refactor: make some changes of struct improve how this popup was built

--- my-homebanking/src/components/common/popup/Popup.js
+++ my-homebanking/src/components/common/popup/Popup.js
@@ -2,25 +2,31 @@ import React from "react";
 import { HeadingSemiBold5 } from "../../../theme/heading/heading";
 import { CloseButton } from "../closeButton/CloseButton";
 import {
-  PopupContainer,
-  PopupContent,
-  PopupHeadContent,
+  Modal,
+  ModalContainer,
+  ModalContent,
+  ModalHeader,
   StyledPopup,
 } from "./stylePopup";
 
 export const Popup = React.forwardRef((props, ref) => {
   return (
-    <StyledPopup ref={ref} modal trigger={props.accion}>
+    <StyledPopup
+      ref={ref}
+      background={props.background}
+      modal
+      trigger={props.accion}
+    >
       {(close) => (
-        <PopupContainer>
-          <PopupContent>
-            <PopupHeadContent>
+        <Modal height={props.height}>
+          <ModalContainer>
+            <ModalHeader>
               <HeadingSemiBold5>{props.headerText}</HeadingSemiBold5>
               <CloseButton propOnClick={close} />
-            </PopupHeadContent>
-            {props.children}
-          </PopupContent>
-        </PopupContainer>
+            </ModalHeader>
+            <ModalContent>{props.children}</ModalContent>
+          </ModalContainer>
+        </Modal>
       )}
     </StyledPopup>
   );
