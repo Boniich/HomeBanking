f9fc6a2603a3a4824fee7ceaf6109390dcd65334
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-26 15:28:55 -0300

    feat: add a global state using context api

--- my-homebanking/src/App.js
+++ my-homebanking/src/App.js
@@ -3,22 +3,30 @@ import "./App.css";
 import Dashboard from "./components/views/dashboard/Dashboard";
 import Layout from "./components/views/layout/Layout";
 import LoginView from "./components/views/login/LoginView";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./routes/privateRoute/PrivateRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Ruta publica */}
-        <Route path="login" element={<LoginView />} />
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Ruta publica */}
 
-        {/* Se deben proteger mas adelante */}
-        <Route path="dashboard" element={<Dashboard />} />
-        {/* Este ruta es provisoria, mas adelante vere si realmente necesito que este aca */}
-        <Route path="transacciones" element={<h1>transacciones</h1>} />
-        {/* error 404 (si no encuentra la pagina) */}
-        <Route path="*" element={<h1>Pagina no encontrada</h1>} />
-      </Route>
-    </Routes>
+          <Route path="login" element={<LoginView />} />
+
+          {/* Se deben proteger mas adelante */}
+          <Route element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+
+          {/* Este ruta es provisoria, mas adelante vere si realmente necesito que este aca */}
+          <Route path="transacciones" element={<h1>transacciones</h1>} />
+          {/* error 404 (si no encuentra la pagina) */}
+          <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
