bdf7e865829de5a756a1f65d6d67ef8156fc1b79
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-30 18:37:29 -0300

    refactor: change of place AuthProvider and AccountProvider from context api

--- my-homebanking/src/App.js
+++ my-homebanking/src/App.js
@@ -3,33 +3,46 @@ import "./App.css";
 import Dashboard from "./components/views/dashboard/Dashboard";
 import Layout from "./components/views/layout/Layout";
 import LoginView from "./components/views/login/LoginView";
+import { AccountProvider } from "./context/accountContext/AccountContext";
 import { AuthProvider } from "./context/AuthContext";
 import IsLogged from "./routes/isLogged/IsLogged";
 import PrivateRoute from "./routes/privateRoute/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Ruta publica */}
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Ruta publica */}
 
-          <Route element={<IsLogged />}>
-            <Route path="login" element={<LoginView />} />
-          </Route>
-
-          <Route element={<PrivateRoute />}>
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-
-          <Route element={<PrivateRoute />}>
-            <Route path="transacciones" element={<h1>transacciones</h1>} />
-          </Route>
-          {/* error 404 (si no encuentra la pagina) */}
-          <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+        <Route element={<IsLogged />}>
+          <Route
+            path="login"
+            element={
+              <AuthProvider>
+                <LoginView />
+              </AuthProvider>
+            }
+          />
         </Route>
-      </Routes>
-    </AuthProvider>
+
+        <Route element={<PrivateRoute />}>
+          <Route
+            path="dashboard"
+            element={
+              <AccountProvider>
+                <Dashboard />
+              </AccountProvider>
+            }
+          />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="transacciones" element={<h1>transacciones</h1>} />
+        </Route>
+        {/* error 404 (si no encuentra la pagina) */}
+        <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+      </Route>
+    </Routes>
   );
 }
 
