61ae0a0610f39a0bfba45a430bee69654ab025a7
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-02 14:22:22 -0300

    feat: finish secondary nav

--- my-homebanking/src/components/common/navs/secondaryNav/SecondaryNav.js
+++ my-homebanking/src/components/common/navs/secondaryNav/SecondaryNav.js
@@ -1,27 +1,70 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { neutralColor } from "../../../../theme/colors/colors";
 import { ParagraphSemibold2 } from "../../../../theme/paragraph/paragraph";
+import { Logo } from "../../logo/Logo";
+import {
+  LogoContainer,
+  LogoContent,
+  LogoImage,
+  LogoTitle,
+} from "../../logo/styleLogo";
 
 const Nav = styled.nav`
   display: flex;
   align-items: center;
-  gap: 27px;
+
   width: 100%;
   height: 60px;
+  gap: 363px;
   background: #fff;
+
+  @media screen and (min-width: 1512px) {
+    height: 80px;
+    ${LogoContainer} {
+      display: flex;
+      margin: 0;
+    }
+
+    ${LogoContent} {
+      flex-direction: row;
+      gap: 6.78px;
+    }
+
+    ${LogoImage} {
+      width: 27.13px;
+      height: 27.13px;
+    }
+
+    ${LogoTitle} {
+      margin: 0;
+      font-size: 20.35px;
+      line-height: 24.42px;
+    }
+  }
 `;
 
 const ComeBackArrow = styled(FontAwesomeIcon)`
   width: 24px;
   height: 24px;
   color: ${neutralColor.neutral700};
+  cursor: pointer;
 `;
 
 const ArrowContainer = styled.span`
   margin-left: 24px;
+
+  @media screen and (min-width: 1512px) {
+    margin-left: 123px;
+  }
+`;
+
+const NavContent = styled.div`
+  display: flex;
+  gap: 27px;
 `;
 
 /**
@@ -33,7 +76,7 @@ const ArrowContainer = styled.span`
 export const SecondaryNav = ({ shortNavText, largeNavText }) => {
   const ref = useRef(false);
   const [navText, setNavText] = useState(shortNavText);
-
+  const navigate = useNavigate();
   const switchTextsByWidth = () => {
     const wid = ref.current.offsetWidth;
     console.log(wid);
@@ -43,16 +86,23 @@ export const SecondaryNav = ({ shortNavText, largeNavText }) => {
     }
   };
 
+  const comeBack = () => {
+    navigate(-1);
+  };
+
   useEffect(() => {
     largeNavText && switchTextsByWidth();
   }, []);
 
   return (
     <Nav ref={ref}>
-      <ArrowContainer>
-        <ComeBackArrow icon={faArrowLeft} />
-      </ArrowContainer>
-      <ParagraphSemibold2>{navText}</ParagraphSemibold2>
+      <NavContent>
+        <ArrowContainer>
+          <ComeBackArrow onClick={comeBack} icon={faArrowLeft} />
+        </ArrowContainer>
+        <ParagraphSemibold2>{navText}</ParagraphSemibold2>
+      </NavContent>
+      <Logo />
     </Nav>
   );
 };
