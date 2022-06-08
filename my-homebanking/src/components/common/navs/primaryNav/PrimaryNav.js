ff8e74a04d15db14405b3fefe27a9c75b1a53b80
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-08 12:45:08 -0300

    feat: add primary nav at one individual file in order to be used in differents components

---
+++ my-homebanking/src/components/common/navs/primaryNav/PrimaryNav.js
@@ -0,0 +1,161 @@
+import {
+  faAngleRight,
+  faCaretDown,
+  faHouseChimney,
+  faRightLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import React, { createRef, useContext, useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+import AccountContext from "../../../../context/accountContext/AccountContext";
+import { neutralColor } from "../../../../theme/colors/colors";
+import { FooterText } from "../../../../theme/footer/footer";
+import {
+  ParagraphMedium2,
+  ParagraphSemibold2,
+} from "../../../../theme/paragraph/paragraph";
+import { shadownLG } from "../../../../theme/shadown/shadown";
+import { ArrowIcon } from "../../../../theme/styledIcon/styledIcon";
+import { CloseButton } from "../../closeButton/CloseButton";
+import { Logo } from "../../logo/Logo";
+import noUserImage from "../../../../assets/noUserImage.png";
+import { CloseSessionPopup } from "../../popup/closeSessionPopup/CloseSessionPopup";
+import {
+  Li,
+  LiMobile,
+  LinkBox,
+  ListMenu,
+  MenuIcons,
+  Nav,
+  Span,
+  Ul,
+  UlMobile,
+  UserBox,
+  UserIcon,
+  UserImage,
+  UserName,
+} from "./stylePrimaryNav";
+
+export const PrimaryNav = () => {
+  const [responsiveNav, setResponsiveNav] = useState(false);
+  const [changeColorNav, setChangeColorNav] = useState(false);
+  const { name, userImage } = useContext(AccountContext);
+  const refNav = useRef(false);
+  const ref = createRef();
+
+  const openResponsiveNav = () => {
+    setResponsiveNav(!responsiveNav);
+    // mala practica pero es una solucion temporal
+    const wid = refNav.current.offsetWidth;
+    wid >= 1512
+      ? (document.body.style.overflow = "auto")
+      : (document.body.style.overflow = "hidden");
+  };
+
+  const closeResponsivenav = () => {
+    setResponsiveNav(false);
+    document.body.style.overflow = "auto";
+  };
+
+  const changeBackgroundColorNav = () => {
+    window.scrollY >= 120 ? setChangeColorNav(true) : setChangeColorNav(false);
+  };
+
+  window.addEventListener("scroll", changeBackgroundColorNav);
+
+  /* Al this props in Nav allow handle the change of background color and font color of nav bar
+    when the user scroll */
+
+  return (
+    <>
+      <Nav
+        ref={refNav}
+        backColor={changeColorNav && "#fff"}
+        titleColor={changeColorNav && `${neutralColor.neutral900}`}
+        fontColor={changeColorNav && `${neutralColor.neutral800}`}
+        iconUserColor={changeColorNav && `${neutralColor.neutral700}`}
+        scrollShadow={changeColorNav && `${shadownLG}`}
+      >
+        <Logo logoColor={!changeColorNav && "changeToWhite"} />
+        <Ul>
+          <Li>
+            <NavLink to="/">
+              {({ isActive }) => (
+                <LinkBox className={isActive ? "active" : ""}>
+                  <MenuIcons icon={faHouseChimney} />
+                  <FooterText>Inicio</FooterText>
+                </LinkBox>
+              )}
+            </NavLink>
+          </Li>
+          <Li off>
+            <FooterText>Tarjetas</FooterText>
+          </Li>
+          <Li>
+            <NavLink to="/transference">
+              {({ isActive }) => (
+                <LinkBox className={isActive ? "active" : ""}>
+                  <MenuIcons icon={faRightLeft} />
+                  <FooterText>Transacciones</FooterText>
+                </LinkBox>
+              )}
+            </NavLink>
+          </Li>
+          <Li>
+            <LinkBox onClick={openResponsiveNav}>
+              <UserBox>
+                <UserImage
+                  src={
+                    userImage
+                      ? `data:image/png;base64,${userImage}`
+                      : noUserImage
+                  }
+                ></UserImage>
+                <UserIcon icon={faCaretDown} />
+              </UserBox>
+              <UserName>{name}</UserName>
+            </LinkBox>
+          </Li>
+        </Ul>
+      </Nav>
+      {responsiveNav && (
+        <ListMenu>
+          <CloseButton propOnClick={closeResponsivenav} />
+          <UlMobile>
+            <LiMobile offOnDesktop>
+              <Span>
+                <ParagraphMedium2>Tarjetas</ParagraphMedium2>
+                <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </LiMobile>
+            <LiMobile offOnDesktop>
+              <Span>
+                <ParagraphMedium2>Transferencias </ParagraphMedium2>
+                <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </LiMobile>
+            <LiMobile>
+              <Span>
+                <ParagraphMedium2>Configuración</ParagraphMedium2>{" "}
+                <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </LiMobile>
+            <LiMobile>
+              <Span>
+                <ParagraphMedium2>Ayuda</ParagraphMedium2>
+                <ArrowIcon icon={faAngleRight}></ArrowIcon>
+              </Span>
+            </LiMobile>
+            <LiMobile>
+              <CloseSessionPopup text="¿Desea cerrar la sesión?">
+                <Span ref={ref}>
+                  <ParagraphSemibold2>Cerrar sesión</ParagraphSemibold2>
+                  <ArrowIcon icon={faAngleRight}></ArrowIcon>
+                </Span>
+              </CloseSessionPopup>
+            </LiMobile>
+          </UlMobile>
+        </ListMenu>
+      )}
+    </>
+  );
+};
