88084209a9cac25652b318c4eeac69d41c8728b2
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-08 09:58:22 -0300

    feat: move two component from close session popup.
    Now this component are re usable

--- my-homebanking/src/components/common/popup/stylePopup.js
+++ my-homebanking/src/components/common/popup/stylePopup.js
@@ -1,8 +1,12 @@
 import Popup from "reactjs-popup";
 import styled from "styled-components";
+import { Caption } from "../../../theme/caption/caption";
 import { neutralColor } from "../../../theme/colors/colors";
-import { HeadingSemiBold5 } from "../../../theme/heading/heading";
-import { shadownLG } from "../../../theme/shadown/shadown";
+import { HeadingBold5, HeadingSemiBold5 } from "../../../theme/heading/heading";
+import {
+  ParagraphMedium2,
+  ParagraphSemibold3,
+} from "../../../theme/paragraph/paragraph";
 import { SendArrowIcon } from "../../../theme/styledIcon/styledIcon";
 import { CloseButtonContainer } from "../closeButton/styledCloseButton";
 import { SendButtonContainer } from "../sendButton/styleSendButton";
@@ -13,7 +17,6 @@ const Modal = styled.div`
   background: #ffffff;
   border-radius: 16px;
   box-shadow: ${(props) => props.shadow || "none"};
-  /* 10px 5px 5px red; */
   @media screen and (min-width: 744px) {
     width: ${(props) => props.tableWidth || `400px`};
     height: ${(props) => props.tableHeight || "auto"};
@@ -68,6 +71,88 @@ const ModalContent = styled.div`
   }
 `;
 
+const ModalSubContent = styled.div.attrs((props) => ({
+  changeGap: props.changeGap && "48px",
+}))`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: ${(props) => props.changeGap || "32px"};
+  margin-top: ${(props) => props.marginTop || "12px"};
+
+  @media screen and (min-width: 744px) {
+    gap: ${(props) => props.changeGap || "64px"};
+    margin-top: ${(props) => props.marginTop || "33px"};
+  }
+
+  @media screen and (min-width: 1512px) {
+    margin-top: ${(props) => props.marginTop || "80px"};
+  }
+`;
+
+const ModalTextAndIcons = styled.div.attrs((props) => ({
+  mobileDisableFlexGap: props.mobileDisableFlexGap === true ? "0" : "24px",
+  tableDisableFlexGap: props.tableDisableFlexGap === true ? "0" : "32px",
+  tableHeadingText: props.tableHeadingText === true ? "24px" : "30px",
+  tableHeadingLineHeight:
+    props.tableHeadingLineHeight === true ? "130%" : "120%",
+  mobileMarginTranf: props.mobileMarginTranf === true ? "16px 0" : "0",
+  tableMarginTranf: props.tableMarginTranf === true ? "21px 0 20px 0" : "0",
+}))`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: ${(props) => props.mobileDisableFlexGap};
+
+  /* only is visible in the transference modal */
+  ${ParagraphMedium2} {
+    color: ${neutralColor.neutral800};
+  }
+
+  ${HeadingBold5} {
+    width: 251px;
+    margin: 0;
+    text-align: center;
+    font-weight: 700;
+    color: ${neutralColor.neutral800};
+    margin: ${(props) => props.mobileMarginTranf};
+  }
+
+  /* only is visible in the transference modal */
+  ${ParagraphSemibold3} {
+    color: ${neutralColor.neutral800};
+    margin-bottom: 3px;
+  }
+
+  ${Caption} {
+    color: ${neutralColor.neutral500};
+  }
+
+  @media screen and (min-width: 744px) {
+    gap: ${(props) => props.tableDisableFlexGap};
+    ${HeadingBold5} {
+      width: 396px;
+      font-size: ${(props) => props.tableHeadingText};
+      line-height: ${(props) => props.tableHeadingLineHeight};
+      letter-spacing: -0.01em;
+      font-weight: 600;
+      margin: ${(props) => props.tableMarginTranf};
+    }
+
+    ${ParagraphMedium2} {
+      font-size: 18px;
+      line-height: 22px;
+    }
+
+    ${ParagraphSemibold3} {
+      font-size: 16px;
+      line-height: 24px;
+    }
+  }
+`;
+
 const StyledPopup = styled(Popup)`
   &-overlay {
     background: ${(props) => props.background || `rgba(17, 24, 39, 0.9)`};
@@ -81,4 +166,12 @@ const StyledPopup = styled(Popup)`
   }
 `;
 
-export { StyledPopup, Modal, ModalContainer, ModalHeader, ModalContent };
+export {
+  StyledPopup,
+  Modal,
+  ModalContainer,
+  ModalHeader,
+  ModalContent,
+  ModalSubContent,
+  ModalTextAndIcons,
+};
