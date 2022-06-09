f59863f38819a825bae95f274a2f645101b11edd
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-09 10:29:51 -0300

    feat: delete file cause name folder was changed

--- my-homebanking/src/components/views/transferences/makeTransference/makeTransfToMyAccount/MakeTransferenceToMyAccount.js
+++ my-homebanking/src/components/views/transferences/makeTransference/makeTransfToMyAccount/MakeTransferenceToMyAccount.js
@@ -3,8 +3,10 @@ import { AccountCard } from "../../../../common/accountCard/AccountCard";
 import { TransferenceForm } from "../commonTransferenceComponents/TransferenceForm.js/TransferenceForm";
 
 export const MakeTransferenceToMyAccount = () => {
+  const shortNavText = "A cuenta propia";
+  const largeNavText = "Transferencia a cuenta propia";
   return (
-    <TransferenceForm>
+    <TransferenceForm shortNavText={shortNavText} largeNavText={largeNavText}>
       <AccountCard />
     </TransferenceForm>
   );
