58b91ac1e8ad48fcfa5b14648d52fb9e465dbd80
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-09 18:31:35 -0300

    feat: add the two input with his containers and the buttons of form

--- my-homebanking/src/components/views/configuration/ConfigurationView.js
+++ my-homebanking/src/components/views/configuration/ConfigurationView.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { ButtonsContainer } from "../../../theme/buttons/buttonContainer/buttonContainer";
-import { ConfigButton } from "../../../theme/buttons/buttons";
+import { Button, ConfigButton } from "../../../theme/buttons/buttons";
 import { ParagraphMedium3 } from "../../../theme/paragraph/paragraph";
 import { shadownMD } from "../../../theme/shadown/shadown";
 import { SecondaryNav } from "../../common/navs/secondaryNav/SecondaryNav";
 import nouUserImage from "../../../assets/noUserImage.png";
 import { errorColor, neutralColor } from "../../../theme/colors/colors";
+import { InputContainer } from "../../../theme/inputs/inputContainer/inputContainer";
+import { Input } from "../../../theme/inputs/input";
 const ConfigurationSection = styled.section`
   display: flex;
   justify-content: center;
@@ -38,12 +40,17 @@ const ConfigurationForm = styled.form`
 const ConfigurationContent = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 24px;
 `;
 
 const ChangeImageContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
+
+  ${ParagraphMedium3} {
+    color: ${neutralColor.neutral700};
+  }
 `;
 
 const ChangeImageContent = styled.div`
@@ -75,6 +82,24 @@ const DeleteImageButton = styled(ConfigButton)`
   width: 76px;
   border-radius: 8px;
   color: ${errorColor.error600};
+`;
+
+const Box1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const SaveChangesButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 18px 24px;
+  margin-top: 8px;
+
+  @media screen and (min-width: 744px) {
+    width: 183px;
+  }
 `;
 
 export const ConfigurationView = () => {
@@ -133,6 +158,18 @@ export const ConfigurationView = () => {
                 </ImageButtonsContainer>
               </ChangeImageContent>
             </ChangeImageContainer>
+            <Box1>
+              <InputContainer>
+                <ParagraphMedium3>Nombres</ParagraphMedium3>
+                <Input type="text" />
+              </InputContainer>
+              <InputContainer>
+                <ParagraphMedium3>Apellidos</ParagraphMedium3>
+                <Input type="text" />
+              </InputContainer>
+            </Box1>
+
+            <SaveChangesButton>Guardar Cambios</SaveChangesButton>
           </ConfigurationContent>
         </ConfigurationForm>
       </ConfigurationSection>
