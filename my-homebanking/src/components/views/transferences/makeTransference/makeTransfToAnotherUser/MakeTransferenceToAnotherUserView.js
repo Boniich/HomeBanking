817bf965e2d33c1825072c3f6a302be92cc692d0
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-06-09 12:01:44 -0300

    feat: pass some props to adapte the style component to the view

--- my-homebanking/src/components/views/transferences/makeTransference/makeTransfToAnotherUser/MakeTransferenceToAnotherUserView.js
+++ my-homebanking/src/components/views/transferences/makeTransference/makeTransfToAnotherUser/MakeTransferenceToAnotherUserView.js
@@ -1,12 +1,12 @@
 import { TransferenceForm } from "../commonTransferenceComponents/TransferenceForm/TransferenceForm";
+import { UserResult } from "../commonTransferenceComponents/userResult/UserResult";
 
 export const MakeTransferenceToAnotherUserView = () => {
   const shortNavText = "A otro usuario";
   const largeNavText = "Transferencia a otro usuario";
   return (
-    <TransferenceForm
-      shortNavText={shortNavText}
-      largeNavText={largeNavText}
-    ></TransferenceForm>
+    <TransferenceForm shortNavText={shortNavText} largeNavText={largeNavText}>
+      <UserResult removeBorder={true} removePadding={true} disableicon={true} />
+    </TransferenceForm>
   );
 };
