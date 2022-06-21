5317d50324cce996371dbfc4efc4f09766f57720
boniich <ezequieldbo22@hotmail.com>
2022-06-21 14:03:44 -0300

    feat: move the function to a folder for re usable functions

---
+++ my-homebanking/src/services/commonFunctions/handleCurrency/handleCurrency.js
@@ -0,0 +1,32 @@
+/**
+ * Handle the string and symbol of each currency
+ * @param {string} currency
+ * @returns {obj}
+ */
+
+export const handleCurrency = (currency) => {
+  let CurrencyType = {
+    currencyText: "",
+    currencySymbol: "",
+  };
+  switch (currency) {
+    case "USD":
+      CurrencyType.currencyText = "Ahorro dolares";
+      CurrencyType.currencySymbol = "$";
+      return CurrencyType;
+    case "ARS":
+      CurrencyType.currencyText = "Ahorro pesos Arg";
+      CurrencyType.currencySymbol = "$";
+      return CurrencyType;
+
+    case "PEN":
+      CurrencyType.currencyText = "Ahorro Soles";
+      CurrencyType.currencySymbol = "S/";
+      return CurrencyType;
+
+    default:
+      CurrencyType.currencyText = "Ahorro";
+      CurrencyType.currencySymbol = "$";
+      return CurrencyType;
+  }
+};
