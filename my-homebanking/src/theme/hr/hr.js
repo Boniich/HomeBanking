37509f7ecbe7680368356214c995e14c029821f2
boniich <ezequieldbo22@hotmail.com>
2022-06-22 14:43:05 -0300

    feat: add a re usable hr

---
+++ my-homebanking/src/theme/hr/hr.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { neutralColor } from "../colors/colors";
+
+export const Hr = styled.hr`
+background: ${neutralColor.neutral200};
+width: 100%;
+height: .1;
+`
