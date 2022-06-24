6d03fdc315025b84cc05fe8ac7206fd4eed0d337
boniich <ezequieldbo22@hotmail.com>
2022-06-24 10:54:16 -0300

    feat: pass by context the states that is necessary to render the data and run the function for save the data

--- my-homebanking/src/components/views/transferences/makeTransference/makeTransfToMyAccount/MakeTransferenceToMyAccount.js
+++ my-homebanking/src/components/views/transferences/makeTransference/makeTransfToMyAccount/MakeTransferenceToMyAccount.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AccountContext from '../../../../../context/accountContext/AccountContext';
+import { DestinyAccountSelect } from '../../../../common/dropdown/destinyAccountSelect/DestinyAccountSelect';
 import { TransferenceForm } from '../commonTransferenceComponents/TransferenceForm/TransferenceForm';
 
 export const MakeTransferenceToMyAccount = () => {
 	const shortNavText = 'A cuenta propia';
 	const largeNavText = 'Transferencia a cuenta propia';
+
+	const {bringOwnAccountToTransfer, searchUserAccNUmber, anotherCurrencyAcc} = useContext(AccountContext);
+
+	useEffect(() =>{
+		bringOwnAccountToTransfer();
+	},[])
+
+	console.log(searchUserAccNUmber);
+
 	return (
 		<TransferenceForm shortNavText={shortNavText} largeNavText={largeNavText}>
-			{/* select especial que contiene las cuentas del usuario */}
+			<DestinyAccountSelect accountNumber={searchUserAccNUmber} currency={anotherCurrencyAcc}/>
 		</TransferenceForm>
 	);
 };
