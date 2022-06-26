e5d687aadd92a0b3367ea67afb18ddb7b9d15954
boniich <ezequieldbo22@hotmail.com>
2022-06-26 14:05:55 -0300

    feat: add some missed color to texts

--- my-homebanking/src/components/common/popup/detailsTransferencePopop/styleDetailsTransferencePopup.js
+++ my-homebanking/src/components/common/popup/detailsTransferencePopop/styleDetailsTransferencePopup.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
+import { neutralColor } from "../../../../theme/colors/colors";
+import { ParagraphMedium2, ParagraphMedium3, ParagraphSemibold2 } from "../../../../theme/paragraph/paragraph";
 
 const TransferenceDetailContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 23px;
+
+    ${ParagraphMedium3}{
+        color: ${neutralColor.neutral500};
+    }
+
+    ${ParagraphMedium2}{
+        color: ${neutralColor.neutral800};
+    }
+
+    ${ParagraphSemibold2}{
+        color: ${neutralColor.neutral800};
+    }
 `;
 
 const DataTransferenceGroup = styled.div`
@@ -16,6 +30,10 @@ const DataTransferenceSubGroup = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 4px;
+
+    ${ParagraphMedium3}{
+        color: ${neutralColor.neutral800};
+    }
 `;
 
 export {
