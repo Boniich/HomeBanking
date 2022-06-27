9b0da51b30185d7ce2a8ec5c2023a174dd3813cf
boniich <ezequieldbo22@hotmail.com>
2022-06-27 19:09:49 -0300

    feat: improve date methods

--- my-homebanking/src/services/commonFunctions/getDate/getDate.js
+++ my-homebanking/src/services/commonFunctions/getDate/getDate.js
@@ -3,6 +3,25 @@ export const getDate = () => {
 	return newDate;
 };
 
+
+const currentDateObj = () =>{
+	const date = new Date();
+	return date;
+}
+
+const transformDateObj = (date) =>{
+	const dateWithProp = new Date(date);
+	return dateWithProp;
+}
+
+export const getCurrentTime = () =>{
+	let time = "";
+	const hour = currentDateObj().getHours();
+	const minutes = currentDateObj().getMinutes();
+	time = `${hour}:${minutes} hs`;
+	return time;
+}
+
 export const transformDateToHumanDate = date => {
 	const allMonths = [
 		'Ene',
@@ -18,19 +37,25 @@ export const transformDateToHumanDate = date => {
 		'Nov',
 		'Dic',
 	];
-	const dateOfToday = getDate();
-	const day = new Date(date).getDate();
-	const monthFromDate = new Date(date).getMonth();
+	const dateOfToday = currentDateObj().toLocaleDateString();
+	const currentDay = currentDateObj().getDate();
+	const currentMonth = currentDateObj().getMonth();
+	const currentYear = currentDateObj().getFullYear();
+
+	const day = transformDateObj(date).getDate();
+	const monthFromDate = transformDateObj(date).getMonth();
 	const month = allMonths.find((e,index) => index === monthFromDate);
-	const year = new Date(date).getFullYear();
-	const hour = new Date(date).getHours();
-	const minutes = new Date(date).getMinutes();
-	const newDate = new Date(date).toLocaleDateString();
+	const year = transformDateObj(date).getFullYear();
+	const hour = transformDateObj(date).getHours();
+	const minutes = transformDateObj(date).getMinutes();
+	const newDate = transformDateObj(date).toLocaleDateString();
 	let result = '';
 	if (dateOfToday === newDate) {
 		result = `Hoy - ${hour}:${minutes} hs`;
-	} else {
-		result = `${day} ${month} ${year} - ${hour}:${minutes} hs`;
+	} else if((currentMonth === monthFromDate) && (currentYear === year) && (currentDay-1 === day)){
+		result = `Ayer - ${hour}:${minutes} hs`
+	}else {
+		result = `${day} ${month}. ${year} - ${hour}:${minutes} hs`;
 	}
 	return result;
 };
