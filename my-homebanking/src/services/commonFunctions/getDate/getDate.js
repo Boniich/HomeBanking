b77342898def9b91e6a19e1be9e482fcec2de990
boniich <ezequieldbo22@hotmail.com>
2022-06-27 15:17:08 -0300

    feat: add the date and a plus like it was in figma

--- my-homebanking/src/services/commonFunctions/getDate/getDate.js
+++ my-homebanking/src/services/commonFunctions/getDate/getDate.js
@@ -4,6 +4,33 @@ export const getDate = () => {
 };
 
 export const transformDateToHumanDate = date => {
+	const allMonths = [
+		'Ene',
+		'Feb',
+		'Mar',
+		'Abr',
+		'May',
+		'Jun',
+		'Jul',
+		'Ago',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dic',
+	];
+	const dateOfToday = getDate();
+	const day = new Date(date).getDate();
+	const monthFromDate = new Date(date).getMonth();
+	const month = allMonths.find((e,index) => index === monthFromDate);
+	const year = new Date(date).getFullYear();
+	const hour = new Date(date).getHours();
+	const minutes = new Date(date).getMinutes();
 	const newDate = new Date(date).toLocaleDateString();
-	return newDate;
+	let result = '';
+	if (dateOfToday === newDate) {
+		result = `Hoy - ${hour}:${minutes} hs`;
+	} else {
+		result = `${day} ${month} ${year} - ${hour}:${minutes} hs`;
+	}
+	return result;
 };
