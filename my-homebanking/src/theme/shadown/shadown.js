48a204caab09c5ad63a7d1c9230789a05a18438f
boniich <ezequieldbo22@hotmail.com>
2022-07-03 19:45:55 -0300

    fix: fix shadow, it was not updated

--- my-homebanking/src/theme/shadown/shadown.js
+++ my-homebanking/src/theme/shadown/shadown.js
@@ -1,7 +1,7 @@
-const shadownXS = `0px 2px 8px rgba(16,71,62,0.11)`;
-const shadownSM = `0px 6px 16px -1px rgba(16,71,62,0.11)`;
-const shadownMD = `0px 9px 20px -2px rgba(16,71,62,0.13)`;
-const shadownLG = `0px 12px 37px -2px rgba(16,71,62,0.09)`;
-const shadownXL = `0px 18px 56px -2px rgba(16,71,62,0.13)`;
+const shadownXS = `0px 2px 10px -2px rgba(9,28,24,0.11)`;
+const shadownSM = `0px 6px 16px -6px rgba(9,28,24,0.15)`;
+const shadownMD = `0px 9px 20px -3px rgba(9,28,24,0.08)`;
+const shadownLG = `0px 12px 37px -8px rgba(9,18,28,0.09)`;
+const shadownXL = `0px 18px 56px -8px rgba(14,28,41,0.09)`;
 
 export { shadownXS, shadownXL, shadownSM, shadownMD, shadownLG };
