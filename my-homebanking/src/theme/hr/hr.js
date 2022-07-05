01a2a2f8c74a57c29be9ac8a919a066d4f5ab310
boniich <ezequieldbo22@hotmail.com>
2022-07-04 22:30:31 -0300

    feat: file deleted

--- my-homebanking/src/theme/hr/hr.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { neutralColor } from '../colors/colors';
-
-export const Hr = styled.hr`
-	background: ${neutralColor.neutral200};
-	width: 100%;
-	height: 0.1;
-`;
